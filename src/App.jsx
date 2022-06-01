326ff48a8b634f63f9abe2ed9ee72f34946ed0d1
Shivom <shvomsrivastava@gmail.com>
2022-06-01 17:43:58 +0530

    🎉 Add files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./index.css";
+const App = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen w-full bg-dark text-light">
+      <h1 className="font-bold text-6xl ">React + Tailwind + Vite Starter</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#222831",
+        gray: "#393E46",
+        primary: "#FFD369",
+        light: "#EEEEEE",
+      },
+    },
+  },
+  plugins: [],
+};
